6807e86bfd939848a54dea361a9bf7cbcfc5820d
loc-dev <nguyentheloc310101@gmail.com>
2024-07-20 13:01:09 +0700

    ci(eslintc): remove rules

---
+++ src/components/dashboard/devices/header-devices.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HeaderDevices = () => {
+  return <div>HeaderDevices</div>;
+};
---
+++ src/components/dashboard/devices/table-devices.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TableDevices = () => {
+  return <div>TableDevices</div>;
+};
